5dd65217f12376bb5ae0973ee7fc8b9e840f1a67
raviwhitehat <ekravi26@gmail.com>
2026-01-19 01:34:59 +0530

    Enhance player and world mechanics with collision layers, new shapes, and improved movement handling

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -5,13 +5,37 @@ extends CharacterBody2D
 @export var Damage := 0
 @export var Speed := 30
 
-func _physics_process(delta: float) -> void:
-	
-	var direction := Input.get_vector("move_left","move_right","move_up","move_down").normalized()
-	
-	if direction != Vector2.ZERO:
-		position += direction * delta * Speed
-		$AnimationPlayer.play("walk")
-	else:
-		$AnimationPlayer.play("Idle")
+@onready var character_sprite: Sprite2D = $CharacterSprite
+
+enum States {Idle, Walk}
+
+var current_state := States.Idle
+
+func _physics_process(_delta: float) -> void:
+	handle_input()
+	handle_movement()
+	handle_animation()
+	handle_sprite_direction()
 	move_and_slide()
+
+func handle_movement():
+	if velocity == Vector2.ZERO:
+		current_state = States.Idle
+	else:
+		current_state = States.Walk
+
+func handle_input() -> void:
+	var direction := Input.get_vector("move_left","move_right","move_up","move_down").normalized()
+	velocity = direction * Speed
+	
+func handle_animation() -> void:
+	if current_state == States.Idle:
+		$AnimationPlayer.play("Idle")
+	if current_state == States.Walk:
+		$AnimationPlayer.play("walk")
+
+func handle_sprite_direction() -> void:
+	if velocity.x > 0:
+		character_sprite.flip_h = false
+	elif velocity.x < 0:
+		character_sprite.flip_h = true
