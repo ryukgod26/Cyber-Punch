0073ba24d7e89074770125eff0160d4a7a2c22fb
raviwhitehat <ekravi26@gmail.com>
2026-01-19 16:57:06 +0530

    Add barrel object and implement player attack animation

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -7,7 +7,7 @@ extends CharacterBody2D
 
 @onready var character_sprite: Sprite2D = $CharacterSprite
 
-enum States {Idle, Walk}
+enum States {Idle, Walk, Attack}
 
 var current_state := States.Idle
 
@@ -19,23 +19,42 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 func handle_movement():
-	if velocity == Vector2.ZERO:
-		current_state = States.Idle
+	if can_move():
+		if velocity == Vector2.ZERO:
+			current_state = States.Idle
+		else:
+			current_state = States.Walk
 	else:
-		current_state = States.Walk
+		velocity = Vector2.ZERO
 
 func handle_input() -> void:
 	var direction := Input.get_vector("move_left","move_right","move_up","move_down").normalized()
 	velocity = direction * Speed
 	
+	if can_attack() and Input.is_action_just_pressed("attack"):
+		current_state = States.Attack
+	
 func handle_animation() -> void:
 	if current_state == States.Idle:
 		$AnimationPlayer.play("Idle")
-	if current_state == States.Walk:
+	elif current_state == States.Walk:
 		$AnimationPlayer.play("walk")
+	elif current_state == States.Attack:
+		$AnimationPlayer.play("punch")
 
 func handle_sprite_direction() -> void:
 	if velocity.x > 0:
 		character_sprite.flip_h = false
 	elif velocity.x < 0:
 		character_sprite.flip_h = true
+
+func can_attack() -> bool:
+	#Might Change the Logic in future due to extra states
+	return current_state == States.Idle or current_state == States.Walk
+
+func can_move() -> bool:
+	#Might Change the Logic in future due to extra states
+	return current_state == States.Idle or current_state == States.Walk
+
+func attack_complete() -> void:
+	current_state = States.Idle
--- Scripts/World/world.gd
+++ Scripts/World/world.gd
@@ -1,9 +1,9 @@
 #class_name World
 extends Node2D
 
-@onready var player: CharacterBody2D = $Entities/Player/Player
+@onready var player: CharacterBody2D = $ActorsContainer/Entities/Player/Player
 @onready var camera: Camera2D = $Camera
 
 func _process(_delta: float) -> void:
 	if player.position.x > camera.position.x:
-		camera.position.x = lerp(camera.position.x,player.position.x,0.2)
+		camera.position.x = lerp(camera.position.x,player.position.x,0.4)
